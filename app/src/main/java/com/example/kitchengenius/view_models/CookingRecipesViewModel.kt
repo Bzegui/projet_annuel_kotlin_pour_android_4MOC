852f039255682cf0ae27140af65d962846b8808a
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-10 23:30:44 +0200

    minor mods

--- app/src/main/java/com/example/kitchengenius/view_models/CookingRecipesViewModel.kt
+++ app/src/main/java/com/example/kitchengenius/view_models/CookingRecipesViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.kitchengenius.view_models
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 /**
  * ViewModel containing the app data and methods to process the data
  */
 class CookingRecipesViewModel : ViewModel() {
-    //private val _uiState = MutableStateFlow(PlayerUiState())
     //val uiState: StateFlow<PlayerUiState> = _uiState.asStateFlow()
 
     /*var playerOneName by mutableStateOf("")
