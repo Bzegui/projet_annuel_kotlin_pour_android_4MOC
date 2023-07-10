b48dd8a1ad7bd2b827f755e791148e526492d9bd
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-10 22:17:29 +0200

    add cooking recipes view model

---
+++ app/src/main/java/com/example/kitchengenius/view_models/CookingRecipesViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.kitchengenius.view_models
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel containing the app data and methods to process the data
+ */
+class CookingRecipesViewModel : ViewModel() {
+    //private val _uiState = MutableStateFlow(PlayerUiState())
+    //val uiState: StateFlow<PlayerUiState> = _uiState.asStateFlow()
+
+    /*var playerOneName by mutableStateOf("")
+        private set*/
+
+    /*
+     * Update player name
+     */
+    /*fun updatePlayerOneName(inputPlayerOneName: String){
+        playerOneName = inputPlayerOneName
+    }*/
+}
