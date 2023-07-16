7731dbd31ac0bb07501c5246df2455f632184645
Zinedine MEGNOUCHE <megnouche.z@gmail.com>
2023-07-16 11:14:17 +0200

    Remove files

--- app/src/main/java/com/example/kitchengenius/presentation/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kitchengenius.presentation.navigation
-
-sealed class Screen(val route: String){
-    object StartScreen: Screen("start")
-    object CookingRecipesScreen: Screen("cookingRecipes")
-}
