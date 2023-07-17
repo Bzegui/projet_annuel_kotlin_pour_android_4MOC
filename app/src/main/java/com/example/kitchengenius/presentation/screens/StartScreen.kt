78ece651a4358a8356daaf07c50329a5bf8f487a
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-17 15:56:29 +0200

    move file

--- app/src/main/java/com/example/kitchengenius/presentation/screens/StartScreen.kt
+++ app/src/main/java/com/example/kitchengenius/presentation/screens/StartScreen.kt
@@ -1,37 +1,31 @@
-package com.example.kitchengenius.presentation
+package com.example.kitchengenius.presentation.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.kitchengenius.view_models.CookingRecipesViewModel
+import androidx.navigation.NavController
 
 @Composable
 fun StartScreen(
-    modifier: Modifier = Modifier,
-    onNavigateToCookingRecipesList: () -> Unit,
-    cookingRecipesViewModel: CookingRecipesViewModel
+    navController: NavController,
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
+
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(20.dp)
+            .padding(50.dp)
+            .wrapContentSize(Alignment.Center),
     ) {
-
-        Button(onClick = onNavigateToCookingRecipesList) {
-            Text(
-                text = "to cooking recipes list",
-            )
-        }
+        Text(text = "Start screen")
     }
 }
+
