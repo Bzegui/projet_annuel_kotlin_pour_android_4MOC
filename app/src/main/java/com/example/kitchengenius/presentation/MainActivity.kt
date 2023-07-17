2b9e28893bd7c2ac062b1e9addba1472fdd4d481
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-17 15:56:15 +0200

    add navigation stack

--- app/src/main/java/com/example/kitchengenius/presentation/MainActivity.kt
+++ app/src/main/java/com/example/kitchengenius/presentation/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.kitchengenius.presentation
 
 import MyAppTheme
-import Theme
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,11 +16,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import com.example.kitchengenius.R
-import com.example.kitchengenius.navigation.NavigationGraph
+import com.example.kitchengenius.presentation.navigation.NavigationStack
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,7 +37,8 @@ class MainActivity : ComponentActivity() {
                         text = stringResource(R.string.app_name),
                         fontSize = 20.sp
                     )
-                    NavigationGraph()
+
+                    NavigationStack()
                 }
             }
 
